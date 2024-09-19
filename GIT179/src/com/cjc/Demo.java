3b8026b1013a0d9123f448760945dddf7b80a350
amitcjc <amitcjcpune@gmail.com>
2024-09-19 17:32:40 +0530

    by Amit2

--- GIT179/src/com/cjc/Demo.java
+++ GIT179/src/com/cjc/Demo.java
@@ -2,9 +2,15 @@ package com.cjc;
 
 public class Demo 
 {
+	public void m1()
+	{
+		System.out.println("Hello M1");
+	}
+	
+	
+	
 	public static void main(String[] args) 
 	{
-		
 		System.out.println("This is Demo Class");
 	}
 	
